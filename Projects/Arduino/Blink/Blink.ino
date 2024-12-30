39c4309d8aa45aac6bb00dc020f1d78e46f3fc99
Virtual Maker <56232456+virtual-maker@users.noreply.github.com>
2024-12-30 19:56:30 +0100

    Test for cppcheck job steps with errors in Blink.ino

--- Projects/Arduino/Blink/Blink.ino
+++ Projects/Arduino/Blink/Blink.ino
@@ -26,6 +26,7 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  ledState = true;
 }
 
 // Loop counter definition
@@ -36,10 +37,10 @@ bool ledState = false;
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
+  delay(1000)/*;*/                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   delay(1000);                      // wait for a second
 
   // Increment the loop counter
   loopCounter++;
-}
+//}
